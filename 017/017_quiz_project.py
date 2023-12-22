4c5349bad0913ea4d409f163d78cb051ea3b9f31
ssjang <s104183@naver.com>
2023-12-22 15:28:09 +0900

    021 snake 완성 231222

--- 017/017_quiz_project.py
+++ 017/017_quiz_project.py
@@ -1,18 +1,40 @@
-class User:
-    def __init__(self, user_id,username):
-        self.id=user_id
-        self.username=username
-        self.followers = 0
-        self.following = 0
-    
-    def follow(self, user):
-        user.followers +=1
-        self.following +=1
-
-user_1 = User('001','angels')
-user_2 = User('002', 'jack')
-
-user_1.follow(user_2)
-
-print(user_1.followers)
-print(user_1.following)
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "class User:\n",
+    "    pass\n",
+    "\n",
+    "user_1 = User()\n",
+    "\n",
+    "def function():\n",
+    "    pass"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.10.4"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
